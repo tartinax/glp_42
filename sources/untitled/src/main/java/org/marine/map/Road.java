9195eb356e253081aade2c88bb4dd1d89d3cb029
GauthierDefrance <defrance.gauthier@gmail.com>
2025-01-21 23:04:23 +0100

    Update Road.java

--- sources/untitled/src/main/java/org/marine/map/Road.java
+++ sources/untitled/src/main/java/org/marine/map/Road.java
@@ -1,4 +1,14 @@
 package org.marine.map;
+import java.awt.Point;
+
+import java.util.HashMap;
 
 public class Road {
+    private Point start;
+    private Point end;
+    private HashMap<Integer, Point> ROAD_POINTS;
+
+    public Road() {}
+
+
 }
