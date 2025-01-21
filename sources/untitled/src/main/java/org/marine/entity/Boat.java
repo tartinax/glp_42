b88b0f640b900a9524a4895f1d7543524309a812
Gauthier Defrance <135462312+GauthierDefrance@users.noreply.github.com>
2025-01-21 16:27:43 +0100

    a

--- sources/untitled/src/main/java/org/marine/entity/Boat.java
+++ sources/untitled/src/main/java/org/marine/entity/Boat.java
@@ -1,4 +1,19 @@
 package org.marine.entity;
+import org.marine.inventory.Inventory;
 
 public class Boat {
+    private int ID;
+    private String NAME;
+    private String Type;
+    private String Model;
+
+    private Inventory inventory;
+
+
+
+    private static int HEALTH_POINT;
+    private int SPEED;
+    private int DAMAGE;
+    
+
 }
